93f26cfbc1901156d13f7f9c50cebef08d2e560f
LazyAdmin2g <69467133+LazyAdmin2g@users.noreply.github.com>
2021-09-13 11:50:33 +0200

    Add files via upload

---
+++ TelefonbuchAusAD_Import.ps1
@@ -0,0 +1,3 @@
+﻿get-aduser -properties * -filter {Mobile -Like "*" -or officephone -Like "*"} -Searchbase "OU=Mitarbeiter,OU=Benutzer,OU=merTens,DC=mertens,DC=ag" | 
+select name, emailaddress,officephone,mobile,Description,l |
+export-csv "M:\00 IT\Projekte\Mitarbeiter Tool  myApp\Import_Files\telefonbuch.csv" -NoTypeInformation -encoding "unicode"
