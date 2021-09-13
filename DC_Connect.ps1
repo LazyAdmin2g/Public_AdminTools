ee4df49b8226529e352087a379fefc5ff7eadfd1
LazyAdmin2g <69467133+LazyAdmin2g@users.noreply.github.com>
2021-09-13 11:45:41 +0200

    Add files via upload

---
+++ DC_Connect.ps1
@@ -0,0 +1,15 @@
+﻿$ExchangeServer = "MAGVEX02"
+$DCServer = "MAGVDC02"
+$FileServer = "MAGVFS02"
+$User = "mag\administrator"
+$Pwd = Read-Host -AsSecureString "Bitte geben Sie das Adminkennwort ein, um sich mit allen benötigten Servern zu verbinden"
+$SecCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pwd
+
+#region Connections
+#Verbindung mit Domaincontroller
+if (!(Get-PSSession | Where { $_.ConfigurationName -eq "Microsoft.PowerShell" })) 
+    { 
+    $DCSession = New-PSSession -ComputerName $DCServer -Credential $SecCred
+    Import-PSSession $DCSession -Module ActiveDirectory -ErrorAction SilentlyContinue -AllowClobber
+    }
+    else{"$DCServer ist bereits verbunden"}
