03964dd3b3f8f32311eb726cba9e09a1e403ad07
LazyAdmin2g <69467133+LazyAdmin2g@users.noreply.github.com>
2021-09-13 11:45:12 +0200

    Add files via upload

---
+++ DateiLaenge.ps1
@@ -0,0 +1,25 @@
+﻿    #Zu prüfendes Verzeichnis oder Volume:
+    $Verzeichnis = „M:\10 Projekte“
+
+    #Pfad zur CSV-Datei für den Export:
+    $CSV = „d:\temp\export.csv“
+
+    #Warnstufe für Pfade über XX Zeichen Laenge in CSV schreiben
+    $Warninglevel = „250“
+
+    #———————————————–
+    „Pfad;Laenge“ | set-content „$CSV“
+    $dirlist = Get-ChildItem „$Verzeichnis“ -recurse | foreach {$_.Fullname}
+    foreach ($dir in $dirlist)
+    {
+    $a = $dir | Measure-Object -Character
+    $length = $a.characters
+
+    if ($length -gt $Warninglevel)
+    {
+    „$dir;$length“ | add-content „$CSV“
+    }
+    }
+    $sort = import-csv „$CSV“ -delimiter „;“ | Sort-Object -Property Laenge -Descending
+    „“ | set-content „$CSV“
+    $sort | export-csv „$CSV“ -delimiter „;“ –NoTypeInformation
