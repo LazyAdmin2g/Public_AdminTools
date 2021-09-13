a0d623a528e1b6feb9912170c6f74fd39458413e
LazyAdmin2g <69467133+LazyAdmin2g@users.noreply.github.com>
2021-09-13 11:46:13 +0200

    Add files via upload

---
+++ EX_Connect.ps1
@@ -0,0 +1,14 @@
+﻿$ExchangeServer = "MAGVEX02"
+$DCServer = "MAGVDC02"
+$FileServer = "MAGVFS02"
+$User = "mag\administrator"
+$Pwd = Read-Host -AsSecureString "Bitte geben Sie das Adminkennwort ein, um sich mit allen benötigten Servern zu verbinden"
+$SecCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pwd
+
+#Verbindung mit Exchange
+if (!(Get-PSSession | Where { $_.ConfigurationName -eq "Microsoft.Exchange" }))
+    {
+    $EXSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/PowerShell/ -Credential $SecCred -Authentication Kerberos 
+    Import-PSSession $EXSession -DisableNameChecking -AllowClobber -ErrorAction SilentlyContinue    
+    }
+    else{"$ExchangeServer ist bereits verbunden"}
