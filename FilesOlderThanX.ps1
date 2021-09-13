75a37a3d40a90ff7e886492d3f81bf97fbb5b25c
LazyAdmin2g <69467133+LazyAdmin2g@users.noreply.github.com>
2021-09-13 11:47:02 +0200

    Add files via upload

---
+++ FilesOlderThanX.ps1
@@ -0,0 +1,12 @@
+﻿$Source = "D:\" # Quelle. Muss mit "\" enden.
+$Days = 180     # Anzahl der Tage
+$log = "$Source$(get-date -format yymmddHHmm).txt"
+$DateBeforeXDays = (Get-Date).AddDays(-$Days)
+
+#Start Script
+Start-Transcript $log
+Write-Host "---------------------------------------------------------"
+Write-Host "Auflisten aller Dateien älter als $Days im Ordner $Source"
+Write-Host "---------------------------------------------------------"
+Get-ChildItem $Source -Recurse | where {$_.LastAccessTime -lt $DateBeforeXDays}
+Stop-Transcript
